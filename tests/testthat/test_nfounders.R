cf57fe280e8eab26a342d3094f3882e5f0c7f79c
Whan, Alex (PI, Black Mountain) <Alex.Whan@csiro.au>
2017-02-25 11:45:10 +1100

    added nfounders tests

---
+++ tests/testthat/test_nfounders.R
@@ -0,0 +1,12 @@
+context("nfounders")
+
+test_that("nfounders returns the correct integer value", {
+  expect_equal(nfounders(m4_cross_qtl), 4)
+  expect_equal(nfounders(bp_cross), 2)
+  expect_equal(nfounders(bp_map), 2)
+})
+
+test_that("nfounders returns error for non map/cross objects") {
+  expect_error(nfounders(mtcars))
+  expect_error(nfounders(4))
+}
