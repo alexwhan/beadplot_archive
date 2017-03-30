0d65ef6b6f5fa4b362aca76ce4f9772b759edad0
Alex Whan <alexwhan@gmail.com>
2017-03-30 15:19:41 +1100

    updated tests and data

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -3,28 +3,24 @@ library(asreml)
 library(mpMap)
 
 set.seed(1234)
-m4_ped <- sim.mpped(4, 1, 500, 6, 1)
-## Dummy QTL to set up structure
-m4_qtl <- matrix(data=c(1, 142, 0.354, -0.354, -0.354, 0.354,
-                                   2, 162, 0.354, -0.354, -0.354, -0.354,
-                                   5, 78, 0.354, -0.354, -0.354, 0.354),
-                            nrow=3, ncol=6, byrow=TRUE)
-m4_cross_qtl <- sim.mpcross(genomap::bp_map, m4_ped, m4_qtl)
+m4_int_qtl <- mpcross2int(genomap::m4_cross, gen.type = "mpInterval")
 
-#Need to rebuild this to use it - too big for data/
-m4_int_qtl <- mpcross2int(m4_cross_qtl, gen.type = "mpInterval")
-
-nqtl.col <- dim(m4_cross_qtl$qtlgeno$finals)[2]
-mmat <- matrix(nrow=500, ncol=4*nqtl.col/2)
+nqtl.col <- dim(genomap::m4_cross$qtlgeno$finals)[2]
+mmat <- matrix(nrow=360, ncol=4*nqtl.col/2)
 for (ii in 1:(nqtl.col/2)) {
-  qtl.fac <- factor(m4_cross_qtl$qtlgeno$finals[,ii])
+  qtl.fac <- factor(genomap::m4_cross$qtlgeno$finals[,ii])
   mmat[,(4*ii-3):(4*ii)] <- model.matrix(~qtl.fac - 1)
 }
 ## Effects
+m4_qtl <- matrix(data=c(1, 142, 0.354, -0.354, -0.354, 0.354,
+                        2, 162, 0.354, -0.354, -0.354, 0.354,
+                        5, 78, 0.354, -0.354, -0.354, 0.354),
+                 nrow=3, ncol=6, byrow=TRUE)
+
 qtl.sizes <- as.vector(t(m4_qtl[,3:6]))
 qtl.effect <- mmat %*% qtl.sizes
 ## Polygenic variance
-pvar <- 0.2
+pvar <- 0.1
 ## Function to calculate approximate percentage variance for each QTL
 perc.var <- function(m4_qtl, poly.var) {
   nfounders <- dim(m4_qtl)[2]-2
@@ -39,18 +35,18 @@ perc.var <- function(m4_qtl, poly.var) {
 percvar <- perc.var(m4_qtl, pvar)
 percvar
 ## Setup simulated data for analysis
-ngeno <- 500
-nrep <- 2
+ngeno <- 360
+nrep <- 3
 id <- factor(rep(paste0("L", 1:ngeno),nrep))
 pheno.data <- data.frame(y = 0, Rep=nrep, id=id)
 set.seed(10)
-ee <- rnorm(ngeno*nrep,0,1)
-uu <- rnorm(ngeno,0,1)
-pheno.data$y <- 10 + rep(qtl.effect,2) + sqrt(1/2)*c(uu,uu) + ee
+ee <- rnorm(ngeno*nrep,0,0.05)
+uu <- rnorm(ngeno,0,0.05)
+pheno.data$y <- 10 + rep(qtl.effect,3) + sqrt(1/2)*c(uu,uu,uu) + ee
 sim.asr0 <- asreml(y ~ 1, random = ~ id, data=pheno.data)
 sim.qtl <- mpwgaim(sim.asr0, pheno.data, m4_int_qtl, merge.by = "id",
                    verboseLev=0, gen.type="interval", na.method.X='include',
                    data.name = "sim.data")
 m4_summary <- summary(sim.qtl, m4_int_qtl)
 
-#save(m4_summary, file = "data/m4_summary.rda")
+save(m4_summary, file = "data/m4_summary.rda")
